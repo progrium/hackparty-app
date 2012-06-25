a7c7422c78ceb2a801f2c80550773f96fd9b1230
Jeff Lindsay <progrium@gmail.com>
2012-06-25 02:50:38 -0700

    initial gdomain module and a view to test it with

--- hackparty/views.py
+++ hackparty/views.py
@@ -1,6 +1,16 @@
+from gdomain import Domain
 from hackparty import app
+
+from google.appengine.api import memcache
+
 from flask import render_template, request
+import json
 
 @app.route('/')
 def hello_world():
     return render_template('helloworld.html')
+
+@app.route('/users')
+def users():
+    d = Domain("hackparty.org", "jeff.lindsay@hackparty.org", memcache.get("domain_pass"))
+    return json.dumps(d.groups.all())
