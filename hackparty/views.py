6306d910895931c1a9c35bcf3c2ab7cdcc047cb2
Ben McGraw <ben.mcgraw@gmail.com>
2012-06-25 03:11:13 -0700

    Do-nothing forms (two of them.)

--- hackparty/views.py
+++ hackparty/views.py
@@ -6,6 +6,20 @@ from google.appengine.api import memcache
 from flask import render_template, request
 import json
 
+@app.route('/preregistration', methods=['GET', 'POST'])
+def preregistration():
+    if request.method == 'GET':
+        return render_template('preregistration.html')
+    if request.method == 'POST':
+        return render_template('helloworld.html')
+
+@app.route('/complete_register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        return render_template('helloworld.html')
+
 @app.route('/')
 def hello_world():
     return render_template('helloworld.html')
